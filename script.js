b01364a9a5b7b3c97d71f40360bffedeae0c70d7
Sarah Tahir <sat454@nyu.edu>
2022-02-22 18:30:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ var button = document.getElementById("clickme"),
   count = 0;
 button.onclick = function() {
   count += 1;
-  document.getElementById("count").innerHTML = count;
+  document.getElementById("count").innerHTML = "Number of times you have interacted with electricity: " + count;
   alert("+1! You interacted with electricity again by pressing the button!");
   count += 1;
-  document.getElementById("count").innerHTML = count;
-  alert("By pressing 'OK,' you interacted with electricity again!");
-  count += 1;
-  document.getElementById("count").innerHTML = count;
+  document.getElementById("count").innerHTML = "Number of times you have interacted with electricity: " + count;
+  while(confirm){
+    alert("By pressing 'OK,' you interacted with electricity again!");
+    count += 1;
+    document.getElementById("count").innerHTML = "Number of times you have interacted with electricity: " + count;
+  }
 };
